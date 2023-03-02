0b123fd9e7709979e5e87771415dd033cf97a878
aantonov65 <aantni048@gmail.com>
2023-03-02 09:11:15 +0200

    added popup

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -9,37 +9,36 @@ import { useNavigate } from "react-router-dom";
 import { Button, Nav } from "react-bootstrap";
 
 const Navigation = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
-    const { userID } = useContext(UserContext);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const { userID } = useContext(UserContext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (typeof userID !== "number") {
-            setIsLoggedIn(false);
-            navigate("/");
-        }
-    }, []);
+  useEffect(() => {
+    if (typeof userID !== "number") {
+      setIsLoggedIn(false);
+      navigate("/");
+    }
+  }, []);
 
-    return (
-        <Navbar
-            collapseOnSelect
-            expand="lg"
-            bg="white"
-            className="position-sticky top-0 w-100 shadow-sm"
-        >
-            <Container>
-                <Navbar.Brand href="/">
-                    <img width={55} src={logo} alt="logo" className="me-2" />
-                    <p className="h4 d-inline">TubeFeel</p>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    {isLoggedIn ? <NavigationLinks /> : null}
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="white"
+      className="position-sticky top-0 w-100 shadow-sm">
+      <Container>
+        <Navbar.Brand href="/">
+          <img width={55} src={logo} alt="logo" className="me-2" />
+          <p className="h4 d-inline">TubeFeel</p>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {isLoggedIn ? <NavigationLinks /> : null}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Navigation;
